2eb7dc50449f68c157a873ef0f0dc0a4298566d1
ccc700 <700eeu@gmail.com>
2024-09-12 20:12:59 +0900

    테ì리스트 테이블 생성

--- fruit_front/src/goods/list.jsx
+++ fruit_front/src/goods/list.jsx
@@ -2,7 +2,26 @@
 function List(){
     return (
         <>
-            list
+            <h2>List</h2>
+            <hr/>
+            <table>
+                <thead>
+                    <tr>
+                        <th>no</th>
+                        <th>item</th>
+                        <th>qty</th>
+                        <th>price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
         </>
     )
 }
