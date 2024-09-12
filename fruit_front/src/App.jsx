357c436548b27df6cdedbdecc5afb9a9eacdf5b7
ccc700 <700eeu@gmail.com>
2024-09-12 19:43:17 +0900

    깃이그노어 추가

--- fruit_front/src/App.jsx
+++ fruit_front/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from "./login.jsx";
+import List from "./goods/list.jsx";
+import Detail from "./goods/detail.jsx";
+import Update from "./goods/admin/update.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-        <h2>hello</h2>
-
-    </>
+      <Router>
+          <Routes>
+              <Route path="/" element={<Login/>}/>
+              <Route path="/goods/list" element={<List/>}/>
+              <Route path="/goods/detail" element={<Detail/>}/>
+              <Route path="/goods/admin/insert" element={<Insert/>}/>
+              <Route path="/goods/admin/update" element={<Update/>}/>
+          </Routes>
+      </Router>
   )
 }
 
