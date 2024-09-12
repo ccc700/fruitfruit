c0f74464b4973cfe26f43a3c9a8f73c0fb5982a9
ccc700 <700eeu@gmail.com>
2024-09-12 20:02:54 +0900

    임포트했음

--- fruit_front/src/App.jsx
+++ fruit_front/src/App.jsx
@@ -7,18 +7,23 @@ import Login from "./login.jsx";
 import List from "./goods/list.jsx";
 import Detail from "./goods/detail.jsx";
 import Update from "./goods/admin/update.jsx";
+import Insert from "./goods/admin/insert.jsx";
 
 function App() {
   return (
-      <Router>
-          <Routes>
-              <Route path="/" element={<Login/>}/>
-              <Route path="/goods/list" element={<List/>}/>
-              <Route path="/goods/detail" element={<Detail/>}/>
-              <Route path="/goods/admin/insert" element={<Insert/>}/>
-              <Route path="/goods/admin/update" element={<Update/>}/>
-          </Routes>
-      </Router>
+      <>
+          <Router>
+              <Routes>
+                  <Route path="/" element={<Login/>}/>
+                  <Route path="/goods/list" element={<List/>}/>
+                  <Route path="/goods/detail" element={<Detail/>}/>
+                  <Route path="/goods/admin/insert" element={<Insert/>}/>
+                  <Route path="/goods/admin/update" element={<Update/>}/>
+              </Routes>
+
+          </Router>
+      </>
+
   )
 }
 
